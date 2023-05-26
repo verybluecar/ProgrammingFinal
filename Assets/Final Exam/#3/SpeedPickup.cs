bbfc3b05d93f650a840e677ba7fcece2430dcbc2
MrPasimio <jpasimio@mvsd320.org>
2023-05-26 06:25:45 -0700

    Update SpeedPickup.cs

--- Assets/Final Exam/#3/SpeedPickup.cs
+++ Assets/Final Exam/#3/SpeedPickup.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/*
+ * Final Exam #3
+ * 
+ *  Make this script inherit the PickUp Class.
+ * Attach this script to the yellow arrow that is on the track
+ * Override the Activate() method to:
+ *      - Save the original speed of the player
+ *      - increase the speed of the car to 5
+ *      - Wait for 2 seconds (Write a coroutine)
+ *      - Change the speed back to the originalSpeed
+ * 
+ */
 public class SpeedPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float originalSpeed;
+    public float boostedSpeed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
